99e2d9847a6e8757a73706a08bc8e5ae436a3bd0
Leonardo Larocca <leonardo_larocca@hotmail.com>
2016-04-13 22:29:30 -0300

    Ajuste trabalho - comp grafica

--- Computação Gráfica/Paleta de Cores/script.js
+++ Computação Gráfica/Paleta de Cores/script.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
 	$('input').mousemove(function(e){
 		if (e.which == 1){
-			var h = document.getElementById('h').value;
-			var s = document.getElementById('s').value;
-			var l = document.getElementById('l').value;
-			//var a = document.getElementById('a').value;
-			var a = 1.0;
-			var cor = ('hsla('+h+','+s+'%,'+l+'%,'+a+')');
-			var sat = "-webkit-linear-gradient(left, hsla("+h+", 0%, 50%, 1), hsla("+h+", 10%, 50%, 1), hsla("+h+", 20%, 50%, 1), hsla("+h+", 30%, 50%, 1), hsla("+h+", 40%, 50%, 1), hsla("+h+", 50%, 50%, 1), hsla("+h+", 60%, 50%, 1), hsla("+h+", 70%, 50%, 1), hsla("+h+", 80%, 50%, 1), hsla("+h+", 90%, 50%, 1), hsla("+h+", 100%, 50%, 1))";
-			var light = "-webkit-linear-gradient(left, hsla("+h+", 100%, 0%, 1), hsla("+h+", 100%, 10%, 1), hsla("+h+", 100%, 20%, 1), hsla("+h+", 100%, 30%, 1), hsla("+h+", 100%, 40%, 1), hsla("+h+", 100%, 50%, 1), hsla("+h+", 100%, 60%, 1), hsla("+h+", 100%, 70%, 1), hsla("+h+", 100%, 80%, 1), hsla("+h+", 100%, 90%, 1), hsla("+h+", 100%, 100%, 1))";
-
-			$(".saturation").css("background",sat);
-			$(".lightness").css("background",light);
-			$('.preview').css("background-color", cor);
-			document.getElementById("resposta").innerHTML = cor;
+			modifica_hsl();
 		}
-
+	});
+	$('input').click(function(){
+		modifica_hsl();
 	});
 });
+
+function modifica_hsl(){
+	var h = document.getElementById('h').value;
+	var s = document.getElementById('s').value;
+	var l = document.getElementById('l').value;
+	var cor = ('hsl('+h+','+s+'%,'+l+'%)');
+	var sat = "-webkit-linear-gradient(left, hsl("+h+", 0%, 50%), hsl("+h+", 10%, 50%), hsl("+h+", 20%, 50%), hsl("+h+", 30%, 50%), hsl("+h+", 40%, 50%), hsl("+h+", 50%, 50%), hsl("+h+", 60%, 50%), hsl("+h+", 70%, 50%), hsl("+h+", 80%, 50%), hsl("+h+", 90%, 50%), hsl("+h+", 100%, 50%))";
+	var light = "-webkit-linear-gradient(left, hsl("+h+", 100%, 0%), hsl("+h+", 100%, 10%), hsl("+h+", 100%, 20%), hsl("+h+", 100%, 30%), hsl("+h+", 100%, 40%), hsl("+h+", 100%, 50%), hsl("+h+", 100%, 60%), hsl("+h+", 100%, 70%), hsl("+h+", 100%, 80%), hsl("+h+", 100%, 90%), hsl("+h+", 100%, 100%))";
+	$(".saturation").css("background",sat);
+	$(".lightness").css("background",light);
+	$('.preview').css("background-color", cor);
+	document.getElementById("resposta").innerHTML = cor;
+}
